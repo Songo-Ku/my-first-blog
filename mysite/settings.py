b0cf332f2971249c51be6b8861f21be5a35a8d28
songo-ku <karczewski49@gmail.com>
2021-06-16 12:19:20 +0200

    dodanie registration, komentowanie postow migracja 0002

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,15 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'Europe/Warsaw'
 
-USE_I18N = True
+USE_I18N = True # internationalization
 
-USE_L10N = True
+USE_L10N = True # localization
 
-USE_TZ = True
+USE_TZ = True # 
 
 
 # Static files (CSS, JavaScript, Images)
@@ -119,3 +119,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
